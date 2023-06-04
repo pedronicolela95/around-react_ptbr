b7b70eda30327b6c5ec188922980681255861808
pedronicolela95 <pedronicolela@gmail.com>
2023-06-04 09:46:11 -0300

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
-import api from "../utils/api.js";
+import api from "../utils/Api.js";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function App() {
